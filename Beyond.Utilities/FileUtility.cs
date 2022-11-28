07778bf3f9d9965e9de7b3cd076ec1b54a447194
hamedfathi <hamedfathi@outlook.com>
2022-11-28 08:24:20 +0100

    feat(Utilities): FileUtility added.

---
+++ Beyond.Utilities/FileUtility.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable UnusedMember.Global
+namespace Beyond.Utilities;
+
+public static class FileUtility
+{
+    public static void Copy(string destination, string target)
+    {
+        var directory = Path.GetDirectoryName(target);
+        Directory.CreateDirectory(directory ?? throw new InvalidOperationException());
+        File.Copy(destination, target);
+    }
+}
