59dc4af901c2e5149cb94cd2b2446ed0c3bf4aa5
hamedfathi <hamedfathi@outlook.com>
2022-12-22 13:14:25 +0100

    feat(NotationConverter): new implementation added.

--- Beyond.Utilities/NotationConverter.cs
+++ Beyond.Utilities/NotationConverter.cs
@@ -1,198 +1,277 @@
 ﻿// ReSharper disable UnusedMember.Global
+
 namespace Beyond.Utilities;
 
-public static class NotationConverter
+public static class Notation2Converter
 {
-    public static string CompleteParenthesisOfInfix(string infix)
+    public static string CompleteParenthesisOfInfix(string infix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)", Func<string, int>? precedence = null)
     {
         if (string.IsNullOrWhiteSpace(infix))
             throw new ArgumentException("Value cannot be null or whitespace.", nameof(infix));
-        infix = infix.RemoveWhitespace();
-        var postfix = InfixToPostfix(infix);
-        var newInfix = PostfixToInfix(postfix);
+        infix = infix.RemoveWhitespaces();
+        var postfix = InfixToPostfix(infix, operandPattern, operatorsAndParenthesesPattern, precedence);
+        var newInfix = PostfixToInfix(postfix, operandPattern, operatorsAndParenthesesPattern);
         return $"({newInfix})";
     }
 
-    public static string InfixToPostfix(string infix)
+    public static string InfixToPostfix(string infix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)", Func<string, int>? precedence = null)
     {
         if (string.IsNullOrWhiteSpace(infix))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(infix));
+        if (string.IsNullOrWhiteSpace(operandPattern))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(operandPattern));
+        if (!operandPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.", nameof(operandPattern));
+        if (string.IsNullOrWhiteSpace(operatorsAndParenthesesPattern))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(operatorsAndParenthesesPattern));
+        if (!operatorsAndParenthesesPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.",
+                nameof(operatorsAndParenthesesPattern));
+
+        var parentheses = new[] { "(", ")" };
+        precedence ??= Precedence;
+        var result = string.Empty;
+        string @out;
+        var stack = new Stack<string>();
+        infix = infix.RemoveWhitespaces();
+        var matches = Regex.Matches(infix, $"{operandPattern}|{operatorsAndParenthesesPattern}");
+        foreach (Match match in matches)
         {
-            throw new ArgumentException($"'{nameof(infix)}' cannot be null or whitespace.", nameof(infix));
-        }
-        infix = infix.RemoveWhitespace();
-        var stk = new Stack<char>();
-        var output = "";
-        char @out;
-        foreach (var ch in infix)
-        {
-            if (char.IsLetterOrDigit(ch))
+            var val = match.Value;
+            if (!parentheses.Contains(val) && !Regex.IsMatch(val, operatorsAndParenthesesPattern))
             {
-                output += ch;
+                result += val;
             }
             else
             {
-                switch (ch)
+                if (!parentheses.Contains(val) && Regex.IsMatch(val, operatorsAndParenthesesPattern))
                 {
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                    case '%':
-                    case '^':
-                        while (stk.Count > 0 && Precedence(ch) <= Precedence(stk.Peek()))
-                        {
-                            @out = stk.Peek();
-                            stk.Pop();
-                            output = output + " " + @out;
-                        }
-                        stk.Push(ch);
-                        output += " ";
-                        break;
+                    while (stack.Count > 0 && precedence(val) <= precedence(stack.Peek()))
+                    {
+                        @out = stack.Peek();
+                        stack.Pop();
+                        result = result + " " + @out;
+                    }
 
-                    case '(':
-                        stk.Push(ch);
-                        break;
+                    stack.Push(val);
+                    result += " ";
+                }
+                else if (val == "(")
+                {
+                    stack.Push(val);
+                }
+                else if (val == ")")
+                {
+                    while (stack.Count > 0 && (@out = stack.Peek()) != "(")
+                    {
+                        stack.Pop();
+                        result = result + " " + @out + " ";
+                    }
 
-                    case ')':
-                        while (stk.Count > 0 && (@out = stk.Peek()) != '(')
-                        {
-                            stk.Pop();
-                            output = output + " " + @out + " ";
-                        }
-                        if (stk.Count > 0 && stk.Peek() == '(')
-                            stk.Pop();
-                        break;
+                    if (stack.Count > 0 && stack.Peek() == "(")
+                        stack.Pop();
                 }
             }
         }
-        while (stk.Count > 0)
+
+        while (stack.Count > 0)
         {
-            @out = stk.Peek();
-            stk.Pop();
-            output = output + @out + " ";
+            @out = stack.Peek();
+            stack.Pop();
+            result = result + @out + " ";
         }
-        return output.RemoveWhitespace();
+
+        return result.RemoveWhitespaces();
     }
 
-    public static string InfixToPrefix(string infix)
+    public static string InfixToPrefix(string infix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)", Func<string, int>? precedence = null)
     {
         if (string.IsNullOrWhiteSpace(infix))
         {
             throw new ArgumentException($"'{nameof(infix)}' cannot be null or whitespace.", nameof(infix));
         }
-        infix = infix.RemoveWhitespace();
-        var postfix = InfixToPostfix(infix.Reverse().MirrorParenthesis());
-        var prefix = postfix.Reverse();
+
+        infix = infix.RemoveWhitespaces();
+        var newInput = Regex.Matches(infix, $"{operandPattern}|{operatorsAndParenthesesPattern}")
+            .Select(x => x.Value).Reverse().Aggregate((a, b) => a + b);
+        var postfix = InfixToPostfix(newInput.MirrorParenthesis(), operandPattern, operatorsAndParenthesesPattern,
+            precedence);
+        var prefix = Regex.Matches(postfix, $"{operandPattern}|{operatorsAndParenthesesPattern}")
+            .Select(x => x.Value).Reverse().Aggregate((a, b) => a + b);
         return prefix;
     }
 
-    public static bool IsValidInfix(string infix)
+    public static bool IsValidInfix(string infix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)")
     {
         if (string.IsNullOrWhiteSpace(infix))
             throw new ArgumentException("Value cannot be null or whitespace.", nameof(infix));
 
-        infix = infix.RemoveWhitespace();
-        var condition = IsWellFormatted(infix);
-        var prev = ' ';
-        foreach (var ch in infix)
-        {
-            if (ch is '(' or ')') continue;
+        infix = infix.RemoveWhitespaces();
+        var condition = IsWellFormattedParentheses(infix);
+        var prev = string.Empty;
+        var matches = Regex.Matches(infix, $"{operandPattern}|{operatorsAndParenthesesPattern}");
 
-            var isLetter = char.IsLetter(ch);
-            if (prev != ' ')
+        foreach (Match match in matches)
+        {
+            var val = match.Value;
+
+            if (val is "(" or ")") continue;
+
+            if (prev != string.Empty)
             {
-                if (isLetter == char.IsLetter(prev))
+                var prevOperand = Regex.IsMatch(prev, operandPattern);
+                var valOperand = Regex.IsMatch(val, operandPattern);
+                if (prevOperand == valOperand)
                 {
                     return false;
                 }
-                if (!isLetter == !char.IsLetter(prev))
+
+                var prevOperator = Regex.IsMatch(prev, operatorsAndParenthesesPattern);
+                var valOperator = Regex.IsMatch(val, operatorsAndParenthesesPattern);
+                if (prevOperator == valOperator)
                 {
                     return false;
                 }
             }
-            prev = ch;
+
+            prev = val;
         }
+
         infix = infix.Replace("(", "").Replace(")", "");
-        var condition2 = char.IsLetter(infix[0]) && char.IsLetter(infix[^1]);
+        var condition2 = !Regex.IsMatch(infix[0].ToString(), operatorsAndParenthesesPattern) &&
+                         !Regex.IsMatch(infix[^1].ToString(), operatorsAndParenthesesPattern);
 
         return condition && condition2;
     }
 
-    public static string PostfixToInfix(string postfix)
+    public static string PostfixToInfix(string postfix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)")
     {
         if (string.IsNullOrWhiteSpace(postfix))
             throw new ArgumentException("Value cannot be null or whitespace.", nameof(postfix));
-        postfix = postfix.RemoveWhitespace();
+        if (string.IsNullOrWhiteSpace(operandPattern))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(operandPattern));
+        if (string.IsNullOrWhiteSpace(operatorsAndParenthesesPattern))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(operatorsAndParenthesesPattern));
+
+        if (!operandPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.", nameof(operandPattern));
+        if (!operatorsAndParenthesesPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.",
+                nameof(operatorsAndParenthesesPattern));
+
         var stackOperands = new Stack<string>();
-        foreach (var ch in postfix)
+        postfix = postfix.RemoveWhitespaces();
+        var matches = Regex.Matches(postfix, $"{operandPattern}|{operatorsAndParenthesesPattern}");
+        foreach (Match match in matches)
         {
-            if (char.IsLetter(ch))
+            var val = match.Value;
+            if (!Regex.IsMatch(val, operatorsAndParenthesesPattern))
             {
-                stackOperands.Push(ch.ToString());
+                stackOperands.Push(val);
             }
             else
             {
                 var operandOld = stackOperands.Pop();
                 var operandOlder = stackOperands.Pop();
-                var newStr = $"({operandOlder}{ch}{operandOld})";
+                var newStr = $"({operandOlder}{val}{operandOld})";
                 stackOperands.Push(newStr);
             }
         }
+
         return stackOperands.Count == 1 ? stackOperands.Pop() : string.Empty;
     }
 
-    public static string PostFixToPrefix(string postfix)
+    public static string PostFixToPrefix(string postfix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)", Func<string, int>? precedence = null)
     {
         if (string.IsNullOrWhiteSpace(postfix))
             throw new ArgumentException("Value cannot be null or whitespace.", nameof(postfix));
-        postfix = postfix.RemoveWhitespace();
-        var infix = PostfixToInfix(postfix);
-        var prefix = InfixToPrefix(infix);
+        postfix = postfix.RemoveWhitespaces();
+        var infix = PostfixToInfix(postfix, operandPattern, operatorsAndParenthesesPattern);
+        var prefix = InfixToPrefix(infix, operandPattern, operatorsAndParenthesesPattern, precedence);
         return prefix;
     }
 
-    public static string PrefixToInfix(string prefix)
+    public static string PrefixToInfix(string prefix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)")
     {
-        if (string.IsNullOrWhiteSpace(prefix))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(prefix));
-        prefix = prefix.RemoveWhitespace();
-        prefix = prefix.Reverse();
+        if (prefix == null) throw new ArgumentNullException(nameof(prefix));
+        if (operandPattern == null) throw new ArgumentNullException(nameof(operandPattern));
+        if (operatorsAndParenthesesPattern == null)
+            throw new ArgumentNullException(nameof(operatorsAndParenthesesPattern));
+        if (!operandPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.", nameof(operandPattern));
+        if (!operatorsAndParenthesesPattern.IsValidRegex())
+            throw new ArgumentException("Value is not valid for a regex pattern.",
+                nameof(operatorsAndParenthesesPattern));
+
+        prefix = prefix.RemoveWhitespaces();
         var stackOperands = new Stack<string>();
-        foreach (var ch in prefix)
+
+        prefix = Regex.Matches(prefix, $"{operandPattern}|{operatorsAndParenthesesPattern}")
+            .Select(x => x.Value).Reverse().Aggregate((a, b) => a + b);
+
+        var matches = Regex.Matches(prefix, $"{operandPattern}|{operatorsAndParenthesesPattern}");
+        foreach (Match match in matches)
         {
-            if (char.IsLetter(ch))
+            var val = match.Value;
+            if (Regex.IsMatch(val, operandPattern))
             {
-                stackOperands.Push(ch.ToString());
+                stackOperands.Push(val);
             }
             else
             {
                 var operandOld = stackOperands.Pop();
                 var operandOlder = stackOperands.Pop();
-                var newStr = $"({operandOlder}{ch}{operandOld})";
+                var newStr = $"({operandOlder}{val}{operandOld})";
                 stackOperands.Push(newStr);
             }
         }
 
         if (stackOperands.Count == 1)
         {
-            var infix = stackOperands.Pop().Reverse().MirrorParenthesis();
+            var infix = Regex.Matches(stackOperands.Pop(), $"{operandPattern}|{operatorsAndParenthesesPattern}")
+                .Select(x => x.Value).Reverse().Aggregate((a, b) => a + b);
+            infix = infix.MirrorParenthesis();
             return infix;
         }
+
         return string.Empty;
     }
 
-    public static string PrefixToPostFix(string prefix)
+    public static string PrefixToPostFix(string prefix, string operandPattern = "[a-zA-Z]",
+        string operatorsAndParenthesesPattern = @"\+|-|\*|/|\^|\||&|\(|\)", Func<string, int>? precedence = null)
     {
         if (string.IsNullOrWhiteSpace(prefix))
             throw new ArgumentException("Value cannot be null or whitespace.", nameof(prefix));
-        prefix = prefix.RemoveWhitespace();
+        prefix = prefix.RemoveWhitespaces();
 
-        var infix = PrefixToInfix(prefix);
-        var postfix = InfixToPostfix(infix);
+        var infix = PrefixToInfix(prefix, operandPattern, operatorsAndParenthesesPattern);
+        var postfix = InfixToPostfix(infix, operandPattern, operatorsAndParenthesesPattern, precedence);
         return postfix;
     }
 
-    private static bool IsWellFormatted(string str)
+    private static bool IsValidRegex(this string pattern)
+    {
+        if (string.IsNullOrWhiteSpace(pattern)) return false;
+        try
+        {
+            var _ = Regex.Match("", pattern);
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsWellFormattedParentheses(string str)
     {
         var lastOpen = new Stack<char>();
         foreach (var c in str)
@@ -202,19 +281,32 @@ public static class NotationConverter
                 case ')':
                     if (lastOpen.Count == 0 || lastOpen.Pop() != '(') return false;
                     break;
+
                 case ']':
                     if (lastOpen.Count == 0 || lastOpen.Pop() != '[') return false;
                     break;
+
                 case '}':
                     if (lastOpen.Count == 0 || lastOpen.Pop() != '{') return false;
                     break;
-                case '(': lastOpen.Push(c); break;
-                case '[': lastOpen.Push(c); break;
-                case '{': lastOpen.Push(c); break;
+
+                case '(':
+                    lastOpen.Push(c);
+                    break;
+
+                case '[':
+                    lastOpen.Push(c);
+                    break;
+
+                case '{':
+                    lastOpen.Push(c);
+                    break;
             }
         }
+
         return lastOpen.Count == 0;
     }
+
     private static string MirrorParenthesis(this string str)
     {
         if (string.IsNullOrWhiteSpace(str))
@@ -230,31 +322,25 @@ public static class NotationConverter
                 _ => s
             };
         }
+
         return newStr;
     }
 
-    private static int Precedence(this char op)
+    private static int Precedence(this string op)
     {
-        if (op is '*' or '/' or '%')
+        if (op is "*" or "/" or "%")
             return 3;
-        if (op is '+' or '-')
+        if (op is "+" or "-")
             return 2;
-        if (op == '^')
+        if (op == "^")
             return 1;
         return -1;
     }
 
-    private static string RemoveWhitespace(this string input)
+    private static string RemoveWhitespaces(this string input)
     {
         return new string(input.ToCharArray()
             .Where(c => !char.IsWhiteSpace(c))
             .ToArray());
-    }
-
-    private static string Reverse(this string str)
-    {
-        var charArray = str.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
     }
 }
