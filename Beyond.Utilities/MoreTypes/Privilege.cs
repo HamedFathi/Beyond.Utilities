92576156573ab8a2c0804da388fce631e771300b
hamedfathi <hamedfathi@outlook.com>
2022-12-28 20:32:27 +0100

    feat(MoreTypes): Privilege added.

---
+++ Beyond.Utilities/MoreTypes/Privilege.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable UnusedMember.Global
+namespace Beyond.Utilities.MoreTypes;
+
+public abstract class Privilege<TItem, TUser>
+{
+    public abstract bool CanCreate(TItem item, TUser user);
+    public abstract bool CanDelete(TItem item, TUser user);
+    public abstract bool CanEdit(TItem item, TUser user);
+    public abstract bool CanView(TItem item, TUser user);
+}
