fb6a2adfd03ed6e72f109a373ce6abfecfa523ec
hamedfathi <hamedfathi@outlook.com>
2023-05-18 22:51:44 +0200

    feat: NullValueDictionary added.

---
+++ Beyond.Utilities/MoreTypes/INullValueDictionary.cs
@@ -0,0 +1,6 @@
+﻿namespace Beyond.Utilities.MoreTypes;
+
+public interface INullValueDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKey : notnull
+{
+    new TValue this[TKey key] { get; set; }
+}
---
+++ Beyond.Utilities/MoreTypes/NullValueDictionary.cs
@@ -0,0 +1,20 @@
+﻿namespace Beyond.Utilities.MoreTypes;
+
+public class NullValueDictionary<TKey, TValue> : Dictionary<TKey, TValue>, INullValueDictionary<TKey, TValue> where TKey : notnull
+{
+    private readonly TValue? _defaultValue;
+    public NullValueDictionary()
+    {
+        _defaultValue = default;
+    }
+    public NullValueDictionary(TValue? defaultValue)
+    {
+        _defaultValue = defaultValue;
+    }
+    
+    public new TValue this[TKey key]
+    {
+        get => TryGetValue(key, out var val) ? val : _defaultValue!;
+        set => base[key] = value;
+    }
+}
