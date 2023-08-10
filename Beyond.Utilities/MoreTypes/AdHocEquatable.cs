6b1da87d6bed5e172f92ee308a3ee6cdbf188010
hamedfathi <hamedfathi@outlook.com>
2023-08-10 21:34:40 +0200

    feat(MoreTypes): AdHocEquatable added.

---
+++ Beyond.Utilities/MoreTypes/AdHocEquatable.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace Beyond.Utilities.MoreTypes;
+
+public sealed class AdHocEquatable<T> : IEquatable<T>
+{
+    private readonly Func<T, bool> equals;
+    public AdHocEquatable(Func<T, bool> eq)
+    {
+        equals = eq;
+    }
+    public bool Equals(T? other)
+    {
+        if (other is null)
+            return false;
+        return equals(other);
+    }
+}
